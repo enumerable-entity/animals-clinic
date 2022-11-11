8642a838c7c7a823683549278cdee3cf255203a5
Enumerable-entity <alfred.dc.alpha@gmail.com>
2022-11-11 21:50:23 +0100

    Spring boot init

---
+++ Animals Clinic/src/main/java/host/enumerableentity/animalsclinic/AnimalsClinicApplication.java
@@ -0,0 +1,13 @@
+package host.enumerableentity.animalsclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalsClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalsClinicApplication.class, args);
+    }
+
+}
---
+++ Animals Clinic/src/test/java/host/enumerableentity/animalsclinic/AnimalsClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package host.enumerableentity.animalsclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalsClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
